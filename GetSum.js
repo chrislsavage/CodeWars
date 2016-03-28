47acf1ba616024fa537073a64c476783c50d8eb1
Chris Savage <chrislsavage@gmail.com>
2016-03-28 10:31:15 -0400

    Get Sum

--- GetSum.js
+++ GetSum.js
@@ -1,3 +1,11 @@
+/*
+Given two integers, which can be positive and negative, find the sum of all the
+numbers between including them too and return it. If both numbers are equal return a or b.
+
+Note! a and b are not ordered!
+*/
+
+
 function GetSum( a,b )
 {
    var max = Math.max( a , b);
